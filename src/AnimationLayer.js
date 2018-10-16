5bdfb91c5f54f635355cb1e44effd9eaf18ec11c
JeongSeongJin <jsj0718tjdwl@naver.com>
2018-10-16 13:23:04 +0900

    Success to make Physics with chipmunk!

--- src/AnimationLayer.js
+++ src/AnimationLayer.js
@@ -1,48 +1,52 @@
 var AnimationLayer = cc.Layer.extend({
-    spriteSheet:null,
-    runningAction:null,
-    sprite:null,
-    ctor:function () {
+    spriteSheet: null,
+    runningAction: null,
+    sprite: null,
+    space:null,
+    body:null,
+    shape:null,
+
+    ctor:function (space) {
         this._super();
+        this.space = space;
         this.init();
     },
     init:function () {
         this._super();
 
-        //create sprite sheet
+        // create sprite sheet
         cc.spriteFrameCache.addSpriteFrames(res.runner_plist);
         this.spriteSheet = new cc.SpriteBatchNode(res.runner_png);
         this.addChild(this.spriteSheet);
 
-        //move action
-        var actionTo = new cc.MoveTo(2, cc.p(300, 85));
 
-        //init runningAction
-        var aniFrames = [];
-        for(var i = 0;i<8;i++){
-            var str="runner"+i+".png";
+        // init runningAction
+        var animFrames = [];
+        for (var i = 0; i < 8; i++) {
+            var str = "runner" + i + ".png";
             var frame = cc.spriteFrameCache.getSpriteFrame(str);
-            aniFrames.push(frame);
+            animFrames.push(frame);
         }
 
-        var animation = new cc.Animation(aniFrames,0.1);
+        var animation = new cc.Animation(animFrames, 0.1);
         this.runningAction = new cc.RepeatForever(new cc.Animate(animation));
-        //1. create PhysicsSprite with a sprite frame name
+
+
+        //create runner through physic engine
         this.sprite = new cc.PhysicsSprite("#runner0.png");
         var contentSize = this.sprite.getContentSize();
-        // 2. init the runner physic body
+        // init body
         this.body = new cp.Body(1, cp.momentForBox(1, contentSize.width, contentSize.height));
-        //3. set the position of the runner
         this.body.p = cc.p(g_runnerStartX, g_groundHeight + contentSize.height / 2);
-        //4. apply impulse to the body
         this.body.applyImpulse(cp.v(150, 0), cp.v(0, 0));//run speed
-        //5. add the created body to space
         this.space.addBody(this.body);
-        //6. create the shape for the body
+        //init shape
         this.shape = new cp.BoxShape(this.body, contentSize.width - 14, contentSize.height);
-        //7. add shape to space
         this.space.addShape(this.shape);
-        //8. set body to the physic sprite
+
         this.sprite.setBody(this.body);
+        this.sprite.runAction(this.runningAction);
+
+        this.spriteSheet.addChild(this.sprite);
     }
 });
--- src/PlayScene.js
+++ src/PlayScene.js
@@ -1,31 +1,31 @@
 var PlayScene = cc.Scene.extend({
-	space:null,
-	initPhysics:function(){
-		this.space = new cp.Space();
-		this.space.gravity = cp.v(0,-350);
-
-		var wallBottom = new cp.SegmentShape(this.space.staticBody,
-			cp.v(0,g_groundHeight),
-			cp.v(4294967295,g_groundHeight),
-			0);
-		this.space.addStaticShape(wallBottom);
-	},
-	update:function(){
-		this.space.step(dt);
-	},
-	ctor:function(space){
-		this._super();
-		this.space=space;
-		this.init();
-	},
+    space:null,
+    // init space of chipmunk
+    initPhysics:function() {
+        this.space = new cp.Space();
+        // Gravity
+        this.space.gravity = cp.v(0, -350);
+        // set up Walls
+        var wallBottom = new cp.SegmentShape(this.space.staticBody,
+            cp.v(0, g_groundHeight),// start point
+            cp.v(4294967295, g_groundHeight),// MAX INT:4294967295
+            0);// thickness of wall
+        this.space.addStaticShape(wallBottom);
+    },
     onEnter:function () {
         this._super();
         this.initPhysics();
 
+        //add three layer in the right order
         this.addChild(new BackgroundLayer());
         this.addChild(new AnimationLayer(this.space));
         this.addChild(new StatusLayer());
 
         this.scheduleUpdate();
+
+    },
+    update:function (dt) {
+        // chipmunk step
+        this.space.step(dt);
     }
 });
